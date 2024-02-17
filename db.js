1f6cd860df9a7c040affb7908302e43616cea423
Shubham Goyal <shubhamgoyal7125@gmail.com>
2024-02-18 00:58:29 +0530

    changed mongoURI

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/iNotebook";
+const mongoURI = "mongodb://127.0.0.1:27017/NoteHive_backend";
 // const mongoURI = require("./config/keys").MONGO_URI;
 const connetToMongo = () =>{
     mongoose.connect(mongoURI, ()=>{
