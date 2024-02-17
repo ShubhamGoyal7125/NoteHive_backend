bafe4f40553c4527d56e6115e06843716d66809b
Shubham Goyal <shubhamgoyal7125@gmail.com>
2024-02-18 00:51:25 +0530

    added get in index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Server is working fine");
+});
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
  
