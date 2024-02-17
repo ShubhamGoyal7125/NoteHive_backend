7edbc1cf5ed6f93feb8aa95642c9db1ef2ee019c
Shubham Goyal <shubhamgoyal7125@gmail.com>
2024-02-18 00:39:47 +0530

    update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const mongoURI = "mongodb://127.0.0.1:27017/iNotebook";
-const mongoURI = require("./config/keys").MONGO_URI;
+const mongoURI = "mongodb://127.0.0.1:27017/iNotebook";
+// const mongoURI = require("./config/keys").MONGO_URI;
 const connetToMongo = () =>{
     mongoose.connect(mongoURI, ()=>{
         console.log("Mongo connection successfully!");
